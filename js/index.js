b55730ae0c059d173e8d98d3a4414147aedd4024
liyingying0411 <422037183@qq.com>
2016-07-17 12:20:41 +0800

    360360shop
    360shop

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(function(){
+	var swiper = new Swiper('.banner .swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        // spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+    var swiper = new Swiper('.bottom .swiper-container', {
+        // pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        slidesPerGroup:3,
+        // spaceBetween: 30
+    });
+})
